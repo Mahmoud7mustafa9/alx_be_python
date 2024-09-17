8ba39731def85387579fffc36cf6acc2dfcec6db
Mahmoud7mustafa9 <122825634+Mahmoud7mustafa9@users.noreply.github.com>
2024-09-17 23:33:23 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,7 @@
 number1 = 10
 number2 = 5
 result1 = number1 + number2
-result2 = numbe1 - number2
+result2 = number1 - number2
 result3 = number1 * number2
 print ("Addition of",number1,"and",number2,"is",result1)
 print ("Subtraction of",number1,"and",number2,"is",result2)
