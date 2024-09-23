9d04dcd43d450d35b1f3f80880e4b5c898a350b0
mahmoud moustafa <7odamostafa97@gmail.com>
2024-09-23 16:34:05 +0300

    vbn

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 
-X = int(input('Enter a number to see its multiplication table:'))
+number = int(input('Enter a number to see its multiplication table:'))
 for Y in range(1,11):
-    Z = X * Y
-    print(f'{X} * {Y} = {Z}')
+    Z = number * Y
+    print(f'{number} * {Y} = {Z}')
 5
