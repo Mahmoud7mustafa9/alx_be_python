787ac8a8d4888fce3171809eb998454d7f9b437f
mahmoud moustafa <7odamostafa97@gmail.com>
2024-09-27 18:06:24 +0300

    dfg

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
 
 
-def perform_operation(number1,number2,operate):
-    if operate == "add":
-        return number1 + number2
-    elif operate == "subtract":
-        return number1 - number2
-    elif operate == "multiply":
-        return number1 * number2
-    elif operate == "divide":
-        if number2 == 0:
+def perform_operation(num1,num2,operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
             return "you can not divide those numbers"
         else :
-            return number1 / number2 
+            return num1 / num2 
