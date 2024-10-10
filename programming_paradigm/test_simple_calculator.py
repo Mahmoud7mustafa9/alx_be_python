375080445d2695f2ad1cd15303c00a3ea1a62e5c
mahmoud moustafa <7odamostafa97@gmail.com>
2024-10-10 17:53:34 +0300

    sff

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -20,7 +20,7 @@ class TestCalc (unittest.TestCase):
        self.assertEqual(self.calc.multiply(2,3),6)
        self.assertEqual(self.calc.add(1,3),3)
 
-    def test_dividition(self):
+    def test_division(self):
         self.assertEqual(self.calc.add(6,3),2)
         self.assertEqual(self.calc.divide(1,0),None)
 
