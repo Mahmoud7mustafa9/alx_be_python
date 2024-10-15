6dc95215f6599eba51df381b3af7864917ac5ac4
mahmoud moustafa <7odamostafa97@gmail.com>
2024-10-15 15:00:23 +0300

    cd

---
+++ oop/book_class.py
@@ -0,0 +1,51 @@
+# Requirements for Book Class in book_class.py:
+# Attributes:
+
+# title (str): The title of the book.
+# author (str): The author of the book.
+# year (int): The publication year of the book.
+# Magic Methods:
+
+
+class Book:
+    def __init__(self,title,author,year):
+        self.title= title
+        self.author= author
+        self.year= year
+    
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"     
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    def __del__(self):
+        return f'Deleting {self.title}'
+
+# Constructor (__init__): Initializes a Book instance with title, author, and year.
+# Destructor (__del__): Prints "Deleting (title of the book)" upon object deletion.
+# String Representation (__str__): Returns a string in the format "(title) by (author), published in (year)".
+# Official Representation (__repr__): Returns a string that would recreate the Book instance: f"Book('{self.title}', '{self.author}', {self.year})".
+# Provided for Testing: main.py
+# To test your Book class, use the following main.py script, which demonstrates creating a Book instance and utilizing the implemented magic methods:
+
+# from book_class import Book
+
+# def main():
+#     # Creating an instance of Book
+#     my_book = Book("1984", "George Orwell", 1949)
+
+#     # Demonstrating the __str__ method
+#     print(my_book)  # Expected to use __str__
+
+#     # Demonstrating the __repr__ method
+#     print(repr(my_book))  # Expected to use __repr__
+
+#     # Deleting a book instance to trigger __del__
+#     del my_book
+
+# if __name__ == "__main__":
+#     main()
+# Expected Output:
+# 1984 by George Orwell, published in 1949
+# Book('1984', 'George Orwell', 1949)
+# Deleting 1984
