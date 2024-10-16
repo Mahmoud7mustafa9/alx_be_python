2c1521d2851344060a32fbab4b4af4c1e389f070
mahmoud moustafa <7odamostafa97@gmail.com>
2024-10-16 14:47:08 +0300

    efe

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,36 +1,34 @@
 class Book:
-    
     def __init__(self, title, author):
         self.title = title
         self.author = author
+    def __str__(self):
+        return f'Book:{self.title} by {self.author}'    
 
 
 class EBook(Book):
     def __init__(self, title, author,file_size):
         super().__init__(title, author) 
-        self.file_size = file_size   
+        self.file_size = file_size  
+    def __str__(self):
+        return f'EBook:{self.title} by {self.author}, File formate {self.file_size}'     
 
 class PrintBook(Book):
-    def __init__(self, title, author,page_count):
+    def __init__(self, title,author,page_count):
         super().__init__(title, author) 
         self.page_count = page_count 
+    def __str__(self):
+        return f'EBook:{self.title} by {self.author}, Page count {self.page_count}'    
+
+
+
 class Library:
-    books_list = []
-    def __init__(self,books):
-        self.books = books
+    def __init__(self):
+        self.Books = []
 
-    def add_book(self, book):
-        return book
-    
+    def add_book(self,book):
+       self.books.append(book)
+
     def list_books(self):
-
-
-
-
-my_library = Library()
-classic_book = Book("Pride and Prejudice", "Jane Austen")
-my_library.add_book(classic_book)
-my_library.list_books()
-
-if __name__ == "__main__":
-    main()
+        for book in self.books:
+            print(book)  
--- oop/main.py
+++ oop/main.py
@@ -2,19 +2,39 @@ from book_class import Book
 from polymorphism_demo import Shape, Rectangle, Circle
 import math
 from class_static_methods_demo import Calculator
+from library_system import Book, EBook, PrintBook, Library
 
-# def main():
-#     # Creating an instance of Book
-#     my_book = Book("1984", "George Orwell", 1949)
+def main():
+    # Creating an instance of Book
+    my_book = Book("1984", "George Orwell", 1949)
 
-#     # Demonstrating the __str__ method
-#     print(my_book)  # Expected to use __str__
+    # Demonstrating the __str__ method
+    print(my_book)  # Expected to use __str__
 
-#     # Demonstrating the __repr__ method
-#     print(repr(my_book))  # Expected to use __repr__
+    # Demonstrating the __repr__ method
+    print(repr(my_book))  # Expected to use __repr__
 
-#     # Deleting a book instance to trigger __del__
-#     del my_book
+    # Deleting a book instance to trigger __del__
+    del my_book
+
+
+
+def main():
+    # Create a Library instance
+    my_library = Library()
+
+    # Create instances of each type of book
+    classic_book = Book("Pride and Prejudice", "Jane Austen")
+    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+
+    # Add books to the library
+    my_library.add_book(classic_book)
+    my_library.add_book(digital_novel)
+    my_library.add_book(paper_novel)
+
+    # List all books in the library
+    my_library.list_books()    
 
 
 
