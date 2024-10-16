6093be977a0ef92a9da2eba8b219efa053b8083a
mahmoud moustafa <7odamostafa97@gmail.com>
2024-10-16 14:52:12 +0300

    d

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,7 +3,7 @@ class Book:
         self.title = title
         self.author = author
     def __str__(self):
-        return f'Book:{self.title} by {self.author}'    
+        return f'Book: {self.title} by {self.author}'    
 
 
 class EBook(Book):
@@ -11,14 +11,14 @@ class EBook(Book):
         super().__init__(title, author) 
         self.file_size = file_size  
     def __str__(self):
-        return f'EBook:{self.title} by {self.author}, File formate {self.file_size}'     
+        return f'EBook: {self.title} by {self.author}, File formate: {self.file_size}'     
 
 class PrintBook(Book):
     def __init__(self, title,author,page_count):
         super().__init__(title, author) 
         self.page_count = page_count 
     def __str__(self):
-        return f'EBook:{self.title} by {self.author}, Page count {self.page_count}'    
+        return f'EBook: {self.title} by {self.author}, Page count: {self.page_count}'    
 
 
 
